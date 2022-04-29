5f8e4174e1cdbbf5a8bee69b9ede046363da4bdc
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2022-04-29 10:56:27 +0300

    saving and fetch data via Realm

--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = [Item]()
+    let realm = try! Realm()
+    var items: Results<Item>?
     var selectedCategory: Category? {
         didSet {
-//            loadItems()
+            loadItems()
         }
     }
 
@@ -28,82 +30,82 @@ class ToDoListViewController: UITableViewController {
     //MARK: - DataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return items?.count ?? 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let item = itemArray[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
-        cell.textLabel?.text = item.title
-        cell.accessoryType = item.done ? .checkmark : .none
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        if let item = items?[indexPath.row] {
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.done ? .checkmark : .none
+        } else {
+            cell.textLabel?.text = "No Items Added"
+        }
         return cell
     }
 
     //MARK: - Delegate
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-//        saveItems()
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+    //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        items[indexPath.row].done = !itemArray[indexPath.row].done
+    ////        saveItems()
+    //        tableView.deselectRow(at: indexPath, animated: true)
+    //    }
 
     //MARK: - Action
 
-//    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-//
-//        var textField = UITextField()
-//        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
-//        let action = UIAlertAction(title: "Add Item", style: .default) { action in
-//
-//            let newItem = Item(context: self.contex)
-//            newItem.title = textField.text
-//            newItem.done = false
-//            newItem.parentCategory = self.selectedCategory
-//            self.itemArray.append(newItem)
-//            self.saveItems()
-//
-//        }
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            if textField.text == "" {
+                let alert = UIAlertController(title: "Please Add Item Name", message: "", preferredStyle: .alert)
+                let actionEmptyField = UIAlertAction(title: "Ok", style: .default, handler: .none)
+                alert.addAction(actionEmptyField)
+                self.present(alert, animated: true, completion: nil)
+            } else if let userText = textField.text,
+                      let currentCategory = self.selectedCategory {
+                do {
+                    try self.realm.write{
+                        let newItem = Item()
+                        newItem.title = userText
+                        currentCategory.items.append(newItem)
+                    }
+                } catch {
+                    print("Saving Error \(error)")
+                }
+            }
+            self.tableView.reloadData()
+        }
 
-//        alert.addTextField { alertTextField in
-//            alertTextField.placeholder = "Create new item"
-//            textField = alertTextField
-//        }
-//        alert.addAction(action)
-//        present(alert, animated: true, completion: nil)
-//    }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new Category"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
     //MARK: - Methods
 
-//    func saveItems() {
-//        do {
-//            try contex.save()
-//        } catch {
-//            print("Error saving \(error)")
-//        }
-//        self.tableView.reloadData()
-//    }
+    //    func saveItems() {
+    //        do {
+    //            try contex.save()
+    //        } catch {
+    //            print("Error saving \(error)")
+    //        }
+    //        self.tableView.reloadData()
+    //    }
 
-//    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
-//
-//        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
-//
-//        if let additionalPredicate = predicate {
-//            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
-//        } else {
-//            request.predicate = categoryPredicate
-//        }
-//
-//        do {
-//            itemArray = try contex.fetch(request)
-//            tableView.reloadData()
-//        } catch {
-//            print("Error loading \(error)")
-//        }
-//    }
+    func loadItems() {
+        items = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
+        tableView.reloadData()
+
+    }
 }
 
-    //MARK: - Extensions
+//MARK: - Extensions
 
 //extension ToDoListViewController: UISearchBarDelegate {
 //
